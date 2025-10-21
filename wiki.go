9f1fe992a41fa079fd47b945a4bb7fc1938aa18d
Aiden Egglin <uefadude@gmail.com>
2025-10-20 21:10:53 -0400

    First commit

---
+++ wiki.go
@@ -0,0 +1,20 @@
+package main
+
+import "os"
+
+type Page struct {
+	Title string
+	Body  []byte // byte slice
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := os.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+
+}
