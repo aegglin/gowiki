4bb3bf41b67a4c411f8d7a3b5ae12623ce30a76f
Aiden Egglin <uefadude@gmail.com>
2025-11-10 19:58:29 -0500

    Fix capitalization

--- server.go
+++ server.go
@@ -23,7 +23,7 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
 		http.NotFound(w, r)
-		return "", errors.New("invalid Page Title")
+		return "", errors.New("Invalid Page Title")
 	}
 	return m[2], nil
 }
@@ -41,6 +41,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
 	renderTemplate(w, "view", p)
+
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
